935632fe0219c0fbd2ffbadb4a7b0ca0546b10e0
TadthepNantapan <630112418045@bru.ac.th>
2022-03-09 23:36:08 +0700

    pull

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -2,28 +2,31 @@ import React from "react";
 import Attachment from "./svg/Attachment";
 import '../CSS/Message.css';
 
-const MessageForm = ({ handleSubmit, text, setText, setImg }) => {
+const MessageForm = ({ handleSubmit, text, setText, setImg }) => { //เรียกใช้ตัวแปรจากCPN Home มาใช้ใน messagefrom ได้แก่ handleSubmit, text, setText, setImg
   return (
-    <form className="message_form" onSubmit={handleSubmit}>
+    <form className="message_form" onSubmit={handleSubmit} //ใช้กำหนดให้ function ทำงานเมื่อ form ถูก submit
+    > 
       <label htmlFor="img" title ="คลิ้ก เพื่อส่งรูปภาพของคุณ (เมื่ออัปโหลดแล้วให้กดปุ่มส่งข้อความด้วย)">
-        <Attachment />
+        <Attachment //จะดึงข้อมูลจาก CPN Attachment เพื่อเอาภาพมาใช้แสดงข้อมูล
+        /> 
       </label>
       <input
-        onChange={(e) => setImg(e.target.files[0])}
-        type="file"
-        id="img"
-        accept="image/*"
-        style={{ display: "none" }}
+        onChange={(e) => setImg(e.target.files[0])} //รับข้อมูลไฟล์รูปภาพ
+        type="file" //ข้อมูลประเภทfile
+        id="img" //ไอดีจะเป็น img
+        accept="image/*" //จะเลือกรับข้อมูลที่เป็นรูปภาพ
+        style={{ display: "none" }} //เมื่อเลือกรูปภาพ จะไม่มีการแสดงรูปภาพให้เห็น
       />
       <div>
         <input
-          type="text"
-          placeholder="พิมพ์ข้อความของคุณที่นี้"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
+          type="text" //ข้อมูลประเภทtext
+          placeholder="พิมพ์ข้อความของคุณที่นี้" //จะมีข้อความว่า "พิมพ์ข้อความของคุณที่นี้" แสดงในช่องพิมพ์
+          value={text} //กำหนดค่าเป็นtext
+          onChange={(e) => setText(e.target.value)} //รับข้อความที่ผู้ใช้กำลังพิมพ์
         />
       </div>
-      <div>
+      <div //ปุ่มส่งข้อความ
+      >
         <button className="btnsend" title="กดปุ่มเพื่อส่งข้อความของคุณ">ส่งข้อความ</button>
       </div>
     </form>
--- src/components/User.js
+++ src/components/User.js
@@ -4,12 +4,12 @@ import { onSnapshot, doc } from "firebase/firestore";
 import { db } from "../firebase";
 import '../CSS/User.css'
 
-const User = ({ user1, user, selectUser, chat }) => {
-  const user2 = user?.uid;
-  const [data, setData] = useState("");
+const User = ({ user1, user, selectUser, chat }) => { //
+  const user2 = user?.uid; //เช็คว่าเป็นผู้เข้าใช้คนไหน
+  const [data, setData] = useState(""); //
 
   useEffect(() => {
-    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`; //
     let unsub = onSnapshot(doc(db, "lastMsg", id), (doc) => {
       setData(doc.data());
     });
@@ -20,18 +20,18 @@ const User = ({ user1, user, selectUser, chat }) => {
     <>
       <div
         className={`user_wrapper ${chat.name === user.name && "selected_user"}`}
-        onClick={() => selectUser(user)}
+        onClick={() => selectUser(user)} //ใช้คำสั่งคลิกที่ชื่อแชท เปรียบเทียบชื่อของผู้ใช้กับชื่อที่แสดงในแชทว่าใช่คนเดียวกันไหม และใช้คำสั่งselectUser เพื่อเลือกuserเพื่อสนทนา
       >
         <div className="user_info">
           <div className="user_detail">
             <img src={user.avatar || Img} alt="avatar" className="avatar" />
             <h4>{user.name}</h4>
             {data?.from !== user1 && data?.unread && (
-              <small className="unread">ใหม่</small>
+              <small className="unread">ใหม่</small> //
             )}
           </div>
           <div
-            className={`user_status ${user.isOnline ? "online" : "offline"}`}
+            className={`user_status ${user.isOnline ? "online" : "offline"}`} //คำสั่งเพื่อดูว่าuserมีการเข้าใช้งานอยู่ไหม ถ้าอยู่จะแสดงonline ถ้าไม่จะแสดงoffline
           ></div>
         </div>
         {data && (
@@ -42,7 +42,7 @@ const User = ({ user1, user, selectUser, chat }) => {
         )}
       </div>
       <div
-        onClick={() => selectUser(user)}
+        onClick={() => selectUser(user)} //เมื่อเกิดการคลิกเพื่อใช้งานselectUser 
         className={`sm_container ${chat.name === user.name && "selected_user"}`}
       >
         <img
